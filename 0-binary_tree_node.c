f88577f3a3dd7b4c686f5629c3d74020508bd964
kelldani <87382954+khalildahbane@users.noreply.github.com>
2024-01-03 14:36:41 +0000

    Binary trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,32 +10,19 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newNode;
 
-        if (parent == NULL)
-        {
-                return (NULL);
-        }
-
-        binary_tree_t newnode = malloc(sizeof(binary_tree_t));
-
-        if (newnode == NULL)
-        {
-                return (NULL);
-        }
-
-
-        newnode->n = value;
-        newnode->parent = parent;
-        newnode->left = NULL;
-        newnode->right = NULL;
-
-        binary_tree_ttemp = parent->left;
-        if (temp != NULL)
-        {
-                newnode->left = temp;
-                temp->parent = newnode;
-        }
-        parent->left = newnode;
-
-        return (newnode);
+	if (!parent)
+		return (NULL);
+	newNode = malloc(sizeof(binary_tree_t));
+	if (!newNode)
+		return (NULL);
+	newNode->parent = parent;
+	newNode->n = value;
+	newNode->left = parent->left;
+	newNode->right = NULL;
+	if (parent->left)
+		parent->left->parent = newNode;
+	parent->left = newNode;
+	return (newNode);
 }
