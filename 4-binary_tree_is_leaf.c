20883a603837bdf995d81e293bea7cb4698e36cd
kelldani <87382954+khalildahbane@users.noreply.github.com>
2024-01-04 15:18:59 +0000

    Binary trees

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,13 +7,16 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-        if (!node)
-        {
-            return (0);
-        }
-        if (!node->left && !node->right)
-        {
-            return (1);
-        }
-        return (0);
+
+	if (!node)
+	{
+		return (0);
+	}
+
+	if (!node->left && !node->right)
+	{
+		return (1);
+	}
+
+	return (0);
 }
